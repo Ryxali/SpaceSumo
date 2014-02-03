b1b68a5df13b9f5af675dd41eba35004c9dfd7eb
Alexander Örn <alexanderorn1@gmail.com>
2014-02-03 15:47:24 +0100

    Added EntityImp

---
+++ SpaceSumo/Entity.cpp
@@ -0,0 +1,28 @@
+#include "Entity.h"
+
+
+Entity::Entity(EntityImp* entity):
+	mEntity(entity)
+{
+
+}
+
+Entity::~Entity()
+{
+	delete mEntity;
+}
+
+void Entity::update()
+{
+
+}
+
+void Entity::draw()
+{
+
+}
+
+bool Entity::addEffect()
+{
+
+}
