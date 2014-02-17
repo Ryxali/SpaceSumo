2a5814aec5e0477ebf50a49af92764fab0a04aa5
alexanderorn <alexanderorn1@gmail.com>
2014-02-17 11:46:54 +0100

    prepared merge

--- Libraries/Common/SVector.cpp
+++ Libraries/Common/SVector.cpp
@@ -2,22 +2,28 @@
 #include "SVector.h"
 #include <math.h>
 #include <Box2D\Box2D.h>
+
+
 SVector::SVector(float x, float y) : sf::Vector2f(x, y)
 {
 
 }
+
 SVector::SVector(const PointProtocol &v)  : sf::Vector2f(v.getX(), v.getY())
 {
 
 }
+
 SVector::SVector(const SVector &v) : sf::Vector2f(v.x, v.y)
 {
 
 }
+
 SVector::SVector(const sf::Vector2f &v) : sf::Vector2f(v.x, v.y)
 {
 
 }
+
 SVector::~SVector()
 {
 	x = 0;
