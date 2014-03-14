bf106eb90d5d6064d97566c52ac29a0108b779a5
Everday <ludvig.arnas@gmail.com>
2014-03-14 16:44:08 +0100

    Asteroids fully operational, needs removal

--- SpaceSumo/Asteroid.cpp
+++ SpaceSumo/Asteroid.cpp
@@ -3,13 +3,14 @@
 #include <ResourceManager\RHandle.h>
 #include "EntityType.h"
 
-Asteroid::Asteroid(b2Vec2 speed, b2Vec2 pos, GameData &data) :
-	mBody(data.world, "res/conf/asteroid.cfg", pos.x, pos.y ),
+Asteroid::Asteroid(b2Vec2 speed, b2Vec2 pos, GameData &data, float scale, float speedMulti) :
+	mBody(data.world, "res/conf/maps/miningcolony/asteroid.cfg", pos.x, pos.y, scale),
 	mSprite(res::getTexture("res/img/maps/miningcolony/asteroid.png"), 9.f),
 	mSpeed(speed),
-	mAlive(true)
+	mAlive(true),
+	mScale(scale)
 {
-	mBody.applyLinearImpulse(b2Vec2(mSpeed.x * 100, mSpeed.y * 100), mBody.getWorldCenter(), true);
+	mBody.applyLinearImpulse(b2Vec2(mSpeed.x * speedMulti, mSpeed.y * speedMulti), mBody.getWorldCenter(), true);
 	mBody.setAngularVelocity(5);
 }
 
@@ -24,6 +25,7 @@ void Asteroid::update(GameData &data, GameStateData &gData, int delta)
 
 void Asteroid::draw(RenderList &renderList)
 {
+	mSprite.getSprite().setScale(mScale, mScale);
 	mSprite.getSprite().setOrigin((float)mSprite.getSprite().getTextureRect().width/2,
 		(float)mSprite.getSprite().getTextureRect().height/2);
 	mSprite.getSprite().setPosition( mBody.getPosition().x * PPM, mBody.getPosition().y * PPM );
