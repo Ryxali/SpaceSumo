ec84ad1f0660462e75484d5b074a21a0beb94d40
unknown <a13thera@SD30403.hs.local>
2014-03-18 14:59:33 +0100

    ET death blink

--- SpaceSumo/ETorpedoPickup.cpp
+++ SpaceSumo/ETorpedoPickup.cpp
@@ -2,15 +2,21 @@
 #include "ETorpedoPickup.h"
 #include <ResourceManager\RHandle.h>
 #include "ETorpedoAbility.h"
+#include <iostream>
 
 
 ETorpedoPickup::ETorpedoPickup(b2World& world, std::string bodyData, float x, float y) :
 	PowerUp(),
 	mBody(world, bodyData, x, y),
-	mAnim(res::getTexture("res/img/PowerUp/EnergyTorpedo/powerup.png"), "res/img/powerup/EnergyTorpedo/energytorpedo_pickup.cfg", 5.f)
+	mAnim(res::getTexture("res/img/PowerUp/EnergyTorpedo/powerup.png"), "res/img/powerup/EnergyTorpedo/energytorpedo_pickup.cfg", 5.f),
+	mDuration(25000),
+	mBlink1(800),
+	mBlink2(400),
+	mDraw(true)
 {
 	mBody.getBody()->SetUserData(this);
 	mAnim.getSprite().setOrigin( 32 , 32 );
+	mDuration.reset();
 }
 
 
@@ -22,11 +28,28 @@ void ETorpedoPickup::update(GameData&data, GameStateData& gsData, int delta)
 {
 	mAnim.getSprite().setRotation( mBody.getAngle() * RADIAN_TO_DEGREES );
 	mAnim.getSprite().setPosition( mBody.getWorldCenter().x*PPM, mBody.getWorldCenter().y*PPM);
+
+	if(mDuration.isExpired())
+	{
+		if(mBlink1.isExpired())
+		{
+			mBlink1.reset();
+			mBlink2.reset();
+			mDraw = false;
+		}
+		else if(mBlink2.isExpired())
+		{
+			mDraw = true;
+		}
+	}
 }
 
 void ETorpedoPickup::draw(RenderList& list)
 {
-	list.addSprite(mAnim);
+	if(mDraw)
+	{
+		list.addSprite(mAnim);
+	}
 }
 
 Ability* ETorpedoPickup::getAbility()
