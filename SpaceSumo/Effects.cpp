3e9a18f698d883fa6ba4203163dd5ba866c91121
Ryxali <ryxali@gmail.com>
2014-03-27 18:10:32 +0100

    Optimized effect list
    Switched to a linked list to improve performance when killing effects.

--- SpaceSumo/Effects.cpp
+++ SpaceSumo/Effects.cpp
@@ -3,9 +3,101 @@
 #include "EffectStatus.h"
 #include <Common\error.h>
 #include "Controller.h"
+
 const int Effects::MAX_SIZE = 50;
 
-Effects::Effects() : mSummary(), mCurSize(0)
+class Link
+{
+public:
+	Link(EffectImp* effect);
+	~Link();
+	bool update(Controller& pController, GameData& data, bool overallChange = false);
+	void draw(RenderList& list, SpaceManImp* owner);
+	void add(EffectImp* effect);
+	void evaluate(EffectStatus& status);
+	bool hasExpired();
+	void clear();
+	Link* getNext();
+private:
+	EffectImp* effect;
+	Link* next;
+	
+};
+
+Link::Link(EffectImp* effect) 
+	: effect(effect), next(0) 
+{
+
+}
+
+Link::~Link()
+{
+	delete effect;
+}
+
+bool Link::update(Controller& pController, GameData& data, bool overallChange)
+{
+	effect->update(pController, data);
+	if(next != 0)
+	{
+		next->update(pController, data);
+		if(next->hasExpired())
+		{
+			Link* tmp = next;
+			next = next->getNext();
+			delete tmp;
+			return true;
+		}
+	}
+	return overallChange;
+}
+
+void Link::draw(RenderList& list, SpaceManImp* owner)
+{
+	effect->draw(list, owner);
+	if(next != 0)
+		next->draw(list, owner);
+}
+
+void Link::add(EffectImp* effect)
+{
+	if(next == 0)
+	{
+		next = new Link(effect);
+	}
+	else
+	{
+		next->add(effect);
+	}
+}
+
+void Link::evaluate(EffectStatus& status)
+{
+	status.addFlag(effect);
+	if(next != 0)
+		next->evaluate(status);
+}
+
+bool Link::hasExpired()
+{
+	return !effect->isAlive();
+}
+
+void Link::clear()
+{
+	if(next != 0)
+	{
+		next->clear();
+		delete next;
+	}
+}
+
+Link* Link::getNext()
+{
+	return next;
+}
+
+Effects::Effects() : mSummary(), mCurSize(0), mFirstEffect(0)
 {
 	mEffectList = new EffectImp*[MAX_SIZE];
 	for(int i = 0; i < MAX_SIZE; ++i)
@@ -20,8 +112,21 @@ Effects::~Effects()
 
 void Effects::update(Controller& pController, GameData& data)
 {
-	int totOffset = 0;
 	bool didChange = false;
+	if(mFirstEffect != 0)
+	{
+		didChange = mFirstEffect->update(pController, data);
+		if(mFirstEffect->hasExpired())
+		{
+			Link* tmp = mFirstEffect;
+			mFirstEffect = mFirstEffect->getNext();
+			delete tmp;
+			didChange = true;
+		}
+	}
+	/*
+	int totOffset = 0;
+	
 	for(int i = 0; i < mCurSize; ++i)
 	{
 		mEffectList[i]->update(pController, data);
@@ -32,7 +137,8 @@ void Effects::update(Controller& pController, GameData& data)
 			floatDown(i);
 			didChange = true;
 		}
-	}
+	}*/
+
 	if(didChange)
 	{
 		mSummary = EffectStatus();
@@ -42,29 +148,48 @@ void Effects::update(Controller& pController, GameData& data)
 
 void Effects::draw(RenderList& renderList, SpaceManImp* owner)
 {
+	if(mFirstEffect != 0)
+		mFirstEffect->draw(renderList, owner);
+	/*
 	for(int i = 0; i < mCurSize; i++)
 	{
 		mEffectList[i]->draw(renderList, owner);
-	}
+	}*/
 }
 
 void Effects::addEffect(EffectImp* effect)
 {
+	if(mFirstEffect != 0)
+	{
+		mFirstEffect->add(effect);
+	}
+	else
+	{
+		mFirstEffect = new Link(effect);
+	}
+	/*
 	SAssert(mCurSize < MAX_SIZE, "index out of bounds");
 	mEffectList[mCurSize] = effect;
-	++mCurSize;
+	++mCurSize;*/
 	evaluate();
 }
 
 void Effects::clear()
 {
+	if(mFirstEffect != 0)
+	{
+		mFirstEffect->clear();
+		delete mFirstEffect;
+		mFirstEffect = 0;
+	}
+	/*
 	for(int i = 0; i < mCurSize; ++i)
 	{
 		delete mEffectList[i];
 		mEffectList[i] = 0;
 	}
+	mCurSize = 0;*/
 	mSummary = EffectStatus();
-	mCurSize = 0;
 	evaluate();
 
 }
@@ -76,10 +201,13 @@ EffectStatus& Effects::getStatus()
 
 void Effects::evaluate()
 {
+	if(mFirstEffect != 0)
+		mFirstEffect->evaluate(mSummary);
+	/*
 	for(int i = 0; i < mCurSize; i++)
 	{
 		mSummary.addFlag(mEffectList[i]);
-	}
+	}*/
 }
 
 void Effects::floatDown(int index)
